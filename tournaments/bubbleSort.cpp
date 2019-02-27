04043ebb8be152568b28d4c9ff3e0c766f72cf48
hmducanh <qliphotducanh@gmail.com>
2019-02-27 23:58:46 +0700

    Update bubbleSort.cpp

--- tournaments/bubbleSort.cpp
+++ tournaments/bubbleSort.cpp
@@ -1,6 +1,6 @@
 std::vector<int> bubbleSort(std::vector<int> items) {
 
-  int len = (int)items.size() - 3;
+  int len = (int)items.size();
 
   for (int i = 0; i < len; i++) {
     for (int j = 0, stop = len - i; j < stop - 1; j++) {
